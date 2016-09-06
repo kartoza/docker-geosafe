50f674df6cac1cc652dd8ffbe7c941fe339a345c
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2016-09-06 19:39:19 +0700

    Update configurations for PyCharm support

--- deployment/docker-geonode/setup-geonode.sh
+++ deployment/docker-geonode/setup-geonode.sh
@@ -2,4 +2,8 @@
 
 cp -n /celeryconfig.py /usr/src/geosafe/tasks/headless/celeryconfig.py
 
-python manage.py runserver 0.0.0.0:8000
+if [ $# -eq 1 ] && [ $1 = "dev" ]; then
+    /usr/sbin/sshd -D
+else
+    python manage.py runserver 0.0.0.0:8000
+fi
