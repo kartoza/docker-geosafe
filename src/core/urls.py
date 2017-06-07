30a5c76b84912b213f76a7b2aebd2f7fcabef2b3
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2017-06-08 04:53:14 +0700

    Refactor GeoServer deployment

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,20 +1,18 @@
 # coding=utf-8
 from __future__ import absolute_import
-from django.conf import settings
-from django.conf.urls import include, patterns
+
+from geonode.urls import *
 
 __author__ = 'Rizky Maulana Nugraha <lana.pcfre@gmail.com>'
 __date__ = '8/25/16'
 
-
 pattern_lists = [
     '',
-    # geonode urls
-    (r'', include('geonode.urls')),
 ]
 
 if 'geosafe' in settings.INSTALLED_APPS:
     pattern_lists.append(
         (r'^geosafe/', include('geosafe.urls', namespace="geosafe")))
 
-urlpatterns = patterns(*pattern_lists)
+if pattern_lists:
+    urlpatterns += patterns(*pattern_lists)
