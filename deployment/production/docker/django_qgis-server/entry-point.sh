1be2b13a34ad02253857791598e4e406d4a6b469
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2017-09-11 00:38:13 +0700

    Update django_qgis-server entrypoint

--- deployment/production/docker/django_qgis-server/entry-point.sh
+++ deployment/production/docker/django_qgis-server/entry-point.sh
@@ -9,6 +9,10 @@ paver sync
 echo "Run collectstatic"
 ./manage.py collectstatic --noinput
 
+# Apply permissions
+echo "Apply permissions for QGIS Server"
+chmod 777 geonode/qgis_layer
+chmod 777 geonode/qgis_tiles
 
 if [ "$DEBUG" == "True" ]; then
     python manage.py runserver 0.0.0.0:8000
