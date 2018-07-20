24418924b535d76326784b1ba5368a379b37bc1b
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-07-20 16:20:27 +0700

    Refactor orchestration to use GeoNode default entrypoint

--- deployment/docker-geonode/docker-entrypoint.sh
+++ deployment/docker-geonode/docker-entrypoint.sh
@@ -8,6 +8,30 @@ fi
 
 echo "Number of arguments $#"
 
+#!/bin/bash
+set -e
+
+/usr/local/bin/invoke update >> /usr/src/app/invoke.log
+
+source $HOME/.override_env
+
+echo DATABASE_URL=$DATABASE_URL
+echo GEODATABASE_URL=$GEODATABASE_URL
+echo SITEURL=$SITEURL
+echo ALLOWED_HOSTS=$ALLOWED_HOSTS
+echo GEOSERVER_PUBLIC_LOCATION=$GEOSERVER_PUBLIC_LOCATION
+
+/usr/local/bin/invoke waitfordbs >> /usr/src/app/invoke.log
+
+echo "waitfordbs task done"
+
+/usr/local/bin/invoke migrations >> /usr/src/app/invoke.log
+echo "migrations task done"
+/usr/local/bin/invoke prepare >> /usr/src/app/invoke.log
+echo "prepare task done"
+/usr/local/bin/invoke fixtures >> /usr/src/app/invoke.log
+echo "fixture task done"
+
 if [ $# -eq 1 ]; then
 	case $1 in
 		# Dev mode, allow ssh
